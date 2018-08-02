270ce982ec601cc1b91c50215045a1433352ebe4
Glitch (svg-builder) <none>
2018-08-02 15:38:40 +0000

    🔑🕹 Checkpoint ./public/client.js:867163/1207 ./server.js:867163/1408 ./package.json:867163/5652 ./views/index.html:867163/184

--- public/client.js
+++ public/client.js
@@ -5,6 +5,28 @@ function uploadImage(){
   var fileName = $('#imgFile')[0].files[0].name;
   // console.log(`filename: ${fileName}`);
   var fileExt = fileName.substr(fileName.lastIndexOf('.') + 1).toLowerCase();
-  var validFiles = ["png", "jpg", "jpeg"
+  var validFiles = ["png", "jpg", "jpeg", "bmp"];
+  if (validFiles.includes(fileExt)){
+    var formData = new FormData();
+    formData.append('file', $('#imgFile')[0].files[0]);
+    
+     $.ajax({
+            type: 'POST',
+            url: '/convertImg',
+            data: formData,
+            contentType: false,
+            processData: false,
+            async: false,
+            cache: false,
+            success: function(data){
+              console.log(data);
+              // show SVG on page
+          }
+        });
+    
+  }else{
+    window.alert("Please upload a bitmap image (png, jpg, bmp)");
+    $('#imgFile').val('');
+  }
   
 }
--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express = require('express');
 var app = express();
 
+var tmp = require('tmp');
+var fs = require('fs');
+const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -19,4 +24,37 @@ app.get('/', function(request, response) {
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.POST('/convertImg', function(req, res){
+  if(!req.files) console.log('no files uploaded')
+  else{
+    let imgFile = req.files.file;
+    let imgName = imgFile.name;
+    console.log(`imgName: ${imgName}`);
+    let imgType = imgFile.ext;
+    
+//     tmp.file({postfix: imgType, keep: false, dir: "tmp"}, function _tempFileCreated(err, path, fd, cleanupCallback) {
+//     if (err) throw err;
+//     fs.writeFile(path, stlFile.data, function(err){
+//       console.log('wrote to file!');
+      
+//       // console.log('File: ', path);
+//       // console.log('Filedescriptor: ', fd);
+//       // console.log(`FileName: ${fontName}`);
+
+//       let stl = NodeSTL(path);
+//       let boundingBox = stl.boundingBox; // in millimeters
+//       console.log(stl.boundingBox);  
+//       // convert to inches
+//       boundingBox = boundingBox.map(function(x) {return x * 0.0393701});      
+
+//       cleanupCallback();
+//       // return font name
+      
+//       res.send(boundingBox);
+//   });
+//   });
+  }
+  
 });
